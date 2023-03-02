a8c728377c4f30cebc27fac8304eb0b58bd26034
mouse0429 <mouse0429@naver.com>
2023-03-02 16:49:40 +0900

    230302 PRO

---
+++ programmers/SQL고득점Kit/string_date2.sql
@@ -0,0 +1,8 @@
+-- 조건별로 분류하여 주문상태 출력하기
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, "%Y-%m-%d") AS OUT_DATE, CASE
+        WHEN OUT_DATE IS NULL THEN "출고미정"
+        WHEN OUT_DATE > DATE("2022-05-01") THEN "출고대기"
+        ELSE "출고완료"
+    END AS "출고여부"
+FROM FOOD_ORDER
+ORDER BY ORDER_ID ASC
